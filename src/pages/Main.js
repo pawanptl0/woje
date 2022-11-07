6f51912183b023a2457ffe4f6759df86548ab85e
Pawan Patil <pawanptl0@gmail.com>
2022-11-07 15:19:46 +0530

    height added to main div

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 const Main = () => {
   return (
     <>
-      <div className="d-flex flex-column flex-wrap justify-content-center text-break fw-normal m-auto w-50 py-4">
+      <div className="main-div d-flex flex-column flex-wrap justify-content-center text-break fw-normal mx-auto w-50 py-5">
       <Navbar />
       <h6>saratoga springs, utah 84045</h6>
       
@@ -25,7 +25,8 @@ simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
 
 simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
 
-simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+          simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,1
+          
       </p>
     </div>
     </>
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='mb-2 d-flex flex-column flex-sm-row justify-content-center align-items-center'>
+    <div className='mb-4 d-flex flex-column flex-sm-row justify-content-center align-items-center'>
       <Link to="/" className="btn btn-sm" role="button">Template1</Link>&nbsp;
       <Link to="/templatetwo" className="btn btn-sm" role="button">Template2</Link>&nbsp;
       <Link to="/templatethree" className="btn btn-sm" role="button">Template3</Link>
